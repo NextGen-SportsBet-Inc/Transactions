9b310ad6ca0cea90f704d5f93cd4e1c2c82c9a89
Brums21 <75539238+Brums21@users.noreply.github.com>
2024-06-05 04:18:32 +0100

    fixed messages

--- TransactionsAPI/SharedMessages/Messages.cs
+++ TransactionsAPI/SharedMessages/Messages.cs
@@ -1,6 +1,6 @@
 ﻿namespace Shared.Messages
 {
-    public record UserExistsRequest 
+    public record UserExistsRequest
     {
         public String? UserId { get; set; }
     }
@@ -47,26 +47,14 @@
         public float? AmountToAdd { get; set; }
     }
 
-    public record AddAmountResponse
-    {
-        public bool Success { get; set; }
-
-        public string? ErrorMessage { get; set; }
-
-    }
 
     public record ChangeBetStatusRequest
     {
-        public required string MatchId { get; set; }
+        public required int MatchId { get; set; }
 
-        public required string TeamId { get; set; }
-    }
+        public required int TeamId { get; set; }
 
-    public record ChangeBetStatusResponse
-    {
-        public required string MatchId { get; set; }
-
-        public required string TeamId { get; set; }
+        public required float Odd { get; set; }
     }
 
 }
